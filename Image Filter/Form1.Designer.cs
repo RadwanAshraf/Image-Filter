2ddc9ffd126b47bce0c90a6a28326cbfacd25d5b
RadwanFCIS <radwan.ashraf.fcis@gmail.com>
2018-04-02 12:01:56 +0200

    Add project files.

---
+++ Image Filter/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Image_Filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OpenB = new System.Windows.Forms.Button();
+            this.grayB = new System.Windows.Forms.Button();
+            this.RandomB = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.negativeB = new System.Windows.Forms.Button();
+            this.saveB = new System.Windows.Forms.Button();
+            this.sobelB = new System.Windows.Forms.Button();
+            this.refillColorsConvert = new System.Windows.Forms.Button();
+            this.redB = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenB
+            // 
+            this.OpenB.Location = new System.Drawing.Point(949, 48);
+            this.OpenB.Name = "OpenB";
+            this.OpenB.Size = new System.Drawing.Size(117, 23);
+            this.OpenB.TabIndex = 0;
+            this.OpenB.Text = "Open";
+            this.OpenB.UseVisualStyleBackColor = true;
+            this.OpenB.Click += new System.EventHandler(this.OpenB_Click);
+            // 
+            // grayB
+            // 
+            this.grayB.Location = new System.Drawing.Point(948, 330);
+            this.grayB.Name = "grayB";
+            this.grayB.Size = new System.Drawing.Size(117, 23);
+            this.grayB.TabIndex = 1;
+            this.grayB.Text = "RGB Mask";
+            this.grayB.UseVisualStyleBackColor = true;
+            this.grayB.Click += new System.EventHandler(this.grayB_Click);
+            // 
+            // RandomB
+            // 
+            this.RandomB.Location = new System.Drawing.Point(949, 534);
+            this.RandomB.Name = "RandomB";
+            this.RandomB.Size = new System.Drawing.Size(117, 24);
+            this.RandomB.TabIndex = 2;
+            this.RandomB.Text = "Before/After";
+            this.RandomB.UseVisualStyleBackColor = true;
+            this.RandomB.Click += new System.EventHandler(this.RandomB_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(949, 212);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "0.299";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(990, 212);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "0.287";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1031, 212);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(35, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "0.114";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(944, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "      R          G           B";
+            // 
+            // negativeB
+            // 
+            this.negativeB.Location = new System.Drawing.Point(948, 370);
+            this.negativeB.Name = "negativeB";
+            this.negativeB.Size = new System.Drawing.Size(116, 25);
+            this.negativeB.TabIndex = 9;
+            this.negativeB.Text = "Negative";
+            this.negativeB.UseVisualStyleBackColor = true;
+            this.negativeB.Click += new System.EventHandler(this.negativeB_Click);
+            // 
+            // saveB
+            // 
+            this.saveB.Location = new System.Drawing.Point(949, 565);
+            this.saveB.Name = "saveB";
+            this.saveB.Size = new System.Drawing.Size(117, 23);
+            this.saveB.TabIndex = 10;
+            this.saveB.Text = "Save";
+            this.saveB.UseVisualStyleBackColor = true;
+            this.saveB.Click += new System.EventHandler(this.saveB_Click);
+            // 
+            // sobelB
+            // 
+            this.sobelB.Location = new System.Drawing.Point(948, 441);
+            this.sobelB.Name = "sobelB";
+            this.sobelB.Size = new System.Drawing.Size(117, 23);
+            this.sobelB.TabIndex = 11;
+            this.sobelB.Text = "Sobel Edge";
+            this.sobelB.UseVisualStyleBackColor = true;
+            this.sobelB.Click += new System.EventHandler(this.sopelB_Click);
+            // 
+            // refillColorsConvert
+            // 
+            this.refillColorsConvert.Location = new System.Drawing.Point(947, 467);
+            this.refillColorsConvert.Name = "refillColorsConvert";
+            this.refillColorsConvert.Size = new System.Drawing.Size(117, 23);
+            this.refillColorsConvert.TabIndex = 12;
+            this.refillColorsConvert.Text = "ReFillColors";
+            this.refillColorsConvert.UseVisualStyleBackColor = true;
+            this.refillColorsConvert.Click += new System.EventHandler(this.refillColorsConvert_Click);
+            // 
+            // redB
+            // 
+            this.redB.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.redB.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.redB.FlatAppearance.BorderSize = 0;
+            this.redB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.redB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redB.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.redB.Location = new System.Drawing.Point(947, 158);
+            this.redB.Name = "redB";
+            this.redB.Size = new System.Drawing.Size(119, 35);
+            this.redB.TabIndex = 13;
+            this.redB.Text = "ShowColor";
+            this.redB.UseVisualStyleBackColor = false;
+            this.redB.Click += new System.EventHandler(this.redB_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(914, 664);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(914, 664);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(1117, 705);
+            this.Controls.Add(this.redB);
+            this.Controls.Add(this.refillColorsConvert);
+            this.Controls.Add(this.sobelB);
+            this.Controls.Add(this.saveB);
+            this.Controls.Add(this.negativeB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.RandomB);
+            this.Controls.Add(this.grayB);
+            this.Controls.Add(this.OpenB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenB;
+        private System.Windows.Forms.Button grayB;
+        private System.Windows.Forms.Button RandomB;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button negativeB;
+        private System.Windows.Forms.Button saveB;
+        private System.Windows.Forms.Button sobelB;
+        private System.Windows.Forms.Button refillColorsConvert;
+        private System.Windows.Forms.Button redB;
+    }
+}
+
+
---
+++ Image Filter/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Filter
+{
+    public partial class Form1 : Form
+    {
+        bool f = true;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public Bitmap grayConvert(Bitmap b)
+        {
+            for (int i = 0; i < b.Width; i++)
+                for (int j = 0; j < b.Height; j++)
+                {
+
+                    Color c1 = b.GetPixel(i, j);
+
+                    int r1 = c1.R;
+                    int g1 = c1.G;
+                    int b1 = c1.B;
+
+                    int gray = (byte)(
+                        System.Convert.ToDouble(textBox1.Text) * r1 +
+                        System.Convert.ToDouble(textBox2.Text) * g1 +
+                        System.Convert.ToDouble(textBox3.Text) * b1);
+
+                    r1 = gray;
+                    g1 = gray;
+                    b1 = gray;
+
+
+                    b.SetPixel(i, j, Color.FromArgb(r1, g1, b1));
+
+                }
+            return b;
+        }
+        public Bitmap negativeConvert(Bitmap b)
+        {
+            for (int i = 0; i < b.Width; i++)
+                for (int j = 0; j < b.Height; j++)
+                {
+
+                    Color c1 = b.GetPixel(i, j);
+
+
+                    int a = c1.A;
+                    int r1 = c1.R;
+                    int g1 = c1.G;
+                    int b1 = c1.B;
+
+                    r1 = 255 - r1;
+                    g1 = 255 - g1;
+                    b1 = 255 - b1;
+
+
+                    b.SetPixel(i, j, Color.FromArgb(a, r1, g1, b1));
+
+                }
+            return b;
+        }
+
+        public Bitmap edgeConvert(Bitmap b)
+        {
+
+            int x = 0, y = 0;
+            int valxR = 0, valyR = 0, valxG = 0, valyG = 0, gradiant1, gradiant2, gradiant3;
+            Bitmap temp = new Bitmap(b.Width, b.Height);
+            Color c = Color.FromArgb(0, 0, 0);
+
+            for (int i = 0; i < b.Height; i++)
+                for (int j = 0; j < b.Width; j++)
+                {
+
+                    //for x axis
+                    int[,] gx = new int[3, 3];
+                    int[,] gy = new int[3, 3];
+
+                    gx[0, 0] = -1; gx[0, 1] = 0; gx[0, 2] = 1;
+                    gx[1, 0] = -2; gx[1, 1] = 0; gx[1, 2] = 2;
+                    gx[2, 0] = -1; gx[2, 1] = 0; gx[2, 2] = 1;
+
+                    //////y
+                    gy[0, 0] = -1; gy[0, 1] = -2; gy[0, 2] = -1;
+                    gy[1, 0] = 0; gy[1, 1] = 0; gy[1, 2] = 0;
+                    gy[2, 0] = 1; gy[2, 1] = 2; gy[2, 2] = 1;
+
+
+
+                    if ((i == 0) || i == (b.Height - 1) || (j == 0) || j == (b.Width - 1))
+                    {
+                        c = Color.FromArgb(255, 255, 255);
+                        temp.SetPixel(j, i, c);
+                        valxR = valyR = valxG = valyG = 0;
+
+                    }
+                    else
+                    {
+                        /*  valxG =(b.GetPixel(j - 1, i - 1).G * gx[0, 0])
+                              + (b.GetPixel(j+ 1, i- 1).G * gx[0, 2])
+                              + (b.GetPixel(j - 1, i).G * gx[1, 0])
+                              + (b.GetPixel(j+1, i).G * gx[1, 2])
+                              + (b.GetPixel(j - 1, i + 1).G * gx[2, 0])
+                              + (b.GetPixel(j + 1, i + 1).G * gx[2, 2]);
+
+
+
+                          valyG =(b.GetPixel(j - 1, i - 1).G * gy[0, 0])
+                              + (b.GetPixel(j, i-1).G *  gy[0, 1])
+                              + (b.GetPixel(j+ 1, i- 1).G * gy[0, 2])
+                              + (b.GetPixel(j - 1, i + 1).G * gy[2, 0])
+                              + (b.GetPixel(j, i+1).G *  gy[2, 1])
+                              + (b.GetPixel(j + 1, i + 1).G * gy[2, 2]);
+
+                          */
+                        valxR = (b.GetPixel(j - 1, i - 1).R * gx[0, 0])
+                            + (b.GetPixel(j + 1, i - 1).R * gx[0, 2])
+                            + (b.GetPixel(j - 1, i).R * gx[1, 0])
+                            + (b.GetPixel(j + 1, i).R * gx[1, 2])
+                            + (b.GetPixel(j - 1, i + 1).R * gx[2, 0])
+                            + (b.GetPixel(j + 1, i + 1).R * gx[2, 2]);
+
+
+
+                        valyR = (b.GetPixel(j - 1, i - 1).R * gy[0, 0])
+                            + (b.GetPixel(j, i - 1).R * gy[0, 1])
+                            + (b.GetPixel(j + 1, i - 1).R * gy[0, 2])
+                            + (b.GetPixel(j - 1, i + 1).R * gy[2, 0])
+                            + (b.GetPixel(j, i + 1).R * gy[2, 1])
+                            + (b.GetPixel(j + 1, i + 1).R * gy[2, 2]);
+
+
+
+                        gradiant1 = (int)Math.Sqrt((valxR * valxR) + (valyR * valyR));
+                        //     gradiant1 = (int)Math.Sqrt(gradiant1);
+                        gradiant2 = gradiant1;
+                        if (valxR != 0)
+                            gradiant2 = (int)Math.Abs(Math.Atan(valyR / valxR) * 180);
+                        //                        gradiant1 = (int)Math.Abs(Math.Max(valxG,valxR)) + Math.Abs(Math.Max(valyG, valyR));
+                        // gradiant2 = (int)Math.Abs(Math.Min(valxG, valxR)) + Math.Abs(Math.Min(valyG, valyR));
+
+
+                        // gradiant3 = (int)Math.Abs(Math.Max(valxG, valyG)) + Math.Abs(Math.Max(valyR, valxR));
+
+                        //  gradiant=(int)Math.Sqrt((valx*valx)+(valy*valy));
+
+                        if (gradiant1 < 0)
+                        {
+                            gradiant1 = 0;
+                            //  gradiant2 = 0;
+
+                        }
+
+                        if (gradiant1 > 255)
+                        {
+                            gradiant1 = 255;
+                            // gradiant2 = 255;
+                            /*
+                            Color c1 = b.GetPixel(j, i);
+
+                            int r1 =  c1.R;
+                            int g1 =  c1.G;
+                            int b1 =  c1.B;
+
+                            */
+                            c = Color.FromArgb(gradiant1, gradiant1, gradiant1);
+                            if (gradiant2 <= 30)
+                                c = Color.FromArgb(250, 80, 20);
+                            else if (gradiant2 <= 60)
+                                c = Color.FromArgb(250, 0, 0);
+                            if (gradiant2 <= 60)
+                                c = Color.FromArgb(250, 0, 0);
+                            else if (gradiant2 <= 120)
+                                c = Color.FromArgb(0, 250, 0);
+                            else if (gradiant2 <= 180)
+                                c = Color.FromArgb(0, 0, 250);
+                            else if (gradiant2 <= 270)
+                                c = Color.FromArgb(250, 250, 0);
+                            else if (gradiant2 <= 270)
+                                c = Color.FromArgb(0, 250, 250);
+                            else if (gradiant2 <= 360)
+                                c = Color.FromArgb(250, 0, 250);
+
+
+                            temp.SetPixel(j, i, c);
+
+                            continue;
+                        }
+                        // gradiant1 = 0;
+                        c = Color.FromArgb(gradiant1, gradiant1, gradiant1);
+
+                        //  c = Color.FromArgb(gradiant2, gradiant2, gradiant2);
+
+                        temp.SetPixel(j, i, c);
+
+                    }
+
+
+                }
+            return temp;
+        }
+
+        public Bitmap reColorConvert(Bitmap b)
+        {
+            for (int i = 0; i < b.Width; i++)
+                for (int j = 0; j < b.Height; j++)
+                {
+
+                    Color c1 = b.GetPixel(i, j);
+                    if (c1.R > 200)
+
+                    {
+                        int r1 = System.Convert.ToInt16(textBox1.Text);
+                        int g1 = System.Convert.ToInt16(textBox2.Text);
+                        int b1 = System.Convert.ToInt16(textBox3.Text);
+
+                        b.SetPixel(i, j, Color.FromArgb(r1, g1, b1));
+                    }
+                }
+            return b;
+        }
+
+
+
+        private void OpenB_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofile = new OpenFileDialog();
+            ofile.Filter = "Image File (*.bmp,*.jpg)|*.bmp;*.jpg";
+            if (DialogResult.OK == ofile.ShowDialog())
+            {
+                this.pictureBox1.Image = new Bitmap(ofile.FileName);
+            }
+
+            pictureBox1.BringToFront();
+            pictureBox1.Visible = true;
+        }
+
+
+        private void grayB_Click(object sender, EventArgs e)
+        {
+
+            Bitmap copy = new Bitmap(this.pictureBox1.Image);
+            this.pictureBox2.Image = grayConvert(copy);
+
+            pictureBox2.BringToFront();
+            pictureBox2.Visible = true;
+
+        }
+
+        private void RandomB_Click(object sender, EventArgs e)
+        {
+            if (f)
+            {
+                pictureBox1.BringToFront();
+                pictureBox1.Visible = true;
+                f = false;
+            }
+            else
+            {
+                pictureBox2.BringToFront();
+                pictureBox2.Visible = true;
+                f = true;
+            }
+        }
+
+        private void negativeB_Click(object sender, EventArgs e)
+        {
+            Bitmap copy = new Bitmap(this.pictureBox1.Image);
+            this.pictureBox2.Image = negativeConvert(copy);
+
+            pictureBox2.BringToFront();
+            pictureBox2.Visible = true;
+        }
+
+        private void saveB_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog safd = new SaveFileDialog();
+            safd.Filter = "Image File (*.bmp,*.jpg)|*.bmp;*.jpg";
+            if (safd.ShowDialog() == DialogResult.OK)
+            {
+                this.pictureBox2.Image.Save(safd.FileName, ImageFormat.Jpeg);
+            }
+        }
+
+        private void sopelB_Click(object sender, EventArgs e)
+        {
+
+            Bitmap copy = new Bitmap(this.pictureBox1.Image);
+            this.pictureBox2.Image = edgeConvert(copy);
+
+            pictureBox2.BringToFront();
+            pictureBox2.Visible = true;
+
+        }
+
+        private void refillColorsConvert_Click(object sender, EventArgs e)
+        {
+            Bitmap copy = new Bitmap(this.pictureBox1.Image);
+            this.pictureBox2.Image = reColorConvert(copy);
+
+            pictureBox2.BringToFront();
+            pictureBox2.Visible = true;
+        }
+
+        private void redB_Click(object sender, EventArgs e)
+        {
+            int r1 = System.Convert.ToInt16(textBox1.Text);
+            int g1 = System.Convert.ToInt16(textBox2.Text);
+            int b1 = System.Convert.ToInt16(textBox3.Text);
+            this.redB.BackColor = Color.FromArgb(r1, g1, b1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
